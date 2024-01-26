e421a8d9af613f8a0e200b64c8d94214ff148fab
Alim.dev <alimjivani.dev@gmail.com>
2024-01-26 17:49:17 -0500

    chore: organize initial project folder structure

---
+++ src/controllers/authController.js
@@ -0,0 +1,12 @@
+// src/controllers/authController.js
+const User = require('../models/userModel');
+
+exports.register = async (req, res) => {
+  
+};
+
+exports.login = async (req, res) => {
+};
+
+exports.profile = async (req, res) => {
+};
