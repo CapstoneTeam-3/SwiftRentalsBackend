f36caef5fa108ab1f965f07c30703bdf7f65884d
Alim.dev <alimjivani.dev@gmail.com>
2024-02-08 17:25:28 -0500

    Added update car feature

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -1,14 +1,19 @@
 import Cars from "../models/CarModel.js";
 import Features from "../models/FeatureModel.js";
+import path from "path";
 import multer from "multer";
+import fs from "fs";
 
 const maxFiles = 5;
 const allowedFileTypes = ["image/jpeg", "image/jpg", "image/png", "image/gif"];
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+const datePart = new Date().toISOString().split("T")[0];
 
 const storage = multer.diskStorage({
   destination: "./public/uploads/carimages/",
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, datePart + "-" + file.originalname);
   },
 });
 
@@ -16,7 +21,10 @@ const fileFilter = (req, file, cb) => {
   if (allowedFileTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error("Invalid file type. Allowed types are JPEG, JPG, PNG, GIF."), false);
+    cb(
+      new Error("Invalid file type. Allowed types are JPEG, JPG, PNG, GIF."),
+      false
+    );
   }
 };
 
@@ -25,12 +33,151 @@ const upload = multer({
   fileFilter: fileFilter,
 }).array("images", maxFiles);
 
-
 export const AddCar = async (req, res) => {
   upload(req, res, async (err) => {
     if (err) {
       return res.status(400).json({ error: err.message || "Unknown error" });
     } else {
+      const {
+        make,
+        model,
+        manufacturing_year,
+        is_available,
+        price,
+        description,
+        location,
+        Features,
+      } = req.body;
+
+      try {
+        if (!make || typeof make !== "string") {
+          res.status(400).json({ error: "Car Make is Required" });
+        }
+
+        if (!model || typeof model !== "string") {
+          res.status(400).json({ error: "Car Model is Required" });
+        }
+
+        if (!manufacturing_year) {
+          res.status(400).json({ error: "Car Manufacturing year is Required" });
+        }
+
+        if (!price) {
+          res.status(400).json({ error: "Car Price is Required" });
+        }
+
+        if (!req.files) {
+          res.status(400).json({ error: "Car Image is Required" });
+        } else if (
+          req.files.some((file) => !allowedFileTypes.includes(file.mimetype))
+        ) {
+          return res.status(400).json({
+            error:
+              "Invalid file type(s). Allowed types are JPEG, JPG, PNG, GIF.",
+          });
+        } else if (req.files.length > maxFiles) {
+          return res.status(400).json({
+            error: `Cannot upload more than ${maxFiles} images.`,
+          });
+        }
+
+        if (!description || typeof description !== "string") {
+          res.status(400).json({ error: "Car Description is Required" });
+        }
+
+        if (!location || typeof location !== "string") {
+          res.status(400).json({ error: "Car Location is Required" });
+        }
+
+        if (!Features) {
+          res.status(400).json({ error: "Car Feature is Required" });
+        }
+
+        const userId = req.user.userId;
+        const imagePaths = req.files.map(
+          (file) => datePart + file.originalname
+        );
+        const featuresArray = JSON.parse(Features);
+
+        const newCar = new Cars({
+          make,
+          model,
+          manufacturing_year,
+          is_available,
+          price,
+          User: userId,
+          images: imagePaths,
+          description,
+          location,
+          Features: featuresArray,
+        });
+
+        await newCar.save();
+
+        res.json({ message: "Successfully added New Car." });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    }
+  });
+};
+
+export const GetAllCars = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 10;
+
+    const totalCars = await Cars.countDocuments();
+    const totalPages = Math.ceil(totalCars / pageSize);
+
+    if (page > totalPages) {
+      return res.status(404).json({ error: "Page not found" });
+    }
+
+    const cars = await Cars.find()
+      .populate("Features")
+      .skip((page - 1) * pageSize)
+      .limit(pageSize);
+
+    res.status(200).json({
+      page,
+      totalPages,
+      pageSize,
+      totalCars,
+      cars,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const GetAllCarsImages = (req, res) => {
+  const imageName = req.params.imageName;
+  const currentDirPath = path.resolve(__dirname);
+  const imagePath = path.join(
+    currentDirPath,
+    "../../public/uploads/carimages",
+    imageName
+  );
+
+  res.sendFile(imagePath);
+};
+
+export const UpdateCar = async (req, res) => {
+  upload(req, res, async (err) => {
+    if (err) {
+      return res.status(400).json({ error: err.message || "Unknown error" });
+    }
+
+    const carId = req.params.id;
+    const car = await Cars.findById(carId).populate("Features");
+
+    if (!car) {
+      return res.status(404).json({ error: "Car not found" });
+    }
+
     const {
       make,
       model,
@@ -43,6 +190,7 @@ export const AddCar = async (req, res) => {
     } = req.body;
 
     try {
+
       if (!make || typeof make !== "string") {
         res.status(400).json({ error: "Car Make is Required" });
       }
@@ -51,7 +199,7 @@ export const AddCar = async (req, res) => {
         res.status(400).json({ error: "Car Model is Required" });
       }
 
-      if (!manufacturing_year ) {
+      if (!manufacturing_year) {
         res.status(400).json({ error: "Car Manufacturing year is Required" });
       }
 
@@ -61,9 +209,12 @@ export const AddCar = async (req, res) => {
 
       if (!req.files) {
         res.status(400).json({ error: "Car Image is Required" });
-      } else if (req.files.some(file => !allowedFileTypes.includes(file.mimetype))) {
+      } else if (
+        req.files.some((file) => !allowedFileTypes.includes(file.mimetype))
+      ) {
         return res.status(400).json({
-          error: "Invalid file type(s). Allowed types are JPEG, JPG, PNG, GIF."
+          error:
+            "Invalid file type(s). Allowed types are JPEG, JPG, PNG, GIF.",
         });
       } else if (req.files.length > maxFiles) {
         return res.status(400).json({
@@ -83,82 +234,93 @@ export const AddCar = async (req, res) => {
         res.status(400).json({ error: "Car Feature is Required" });
       }
 
+      
+      const imagePaths = req.files.map((file) => datePart + file.originalname);
+
+      if (car.images && Array.isArray(car.images)) {
+        const oldCarImages = car.images.map((filename) => filename);
+        await DeleteImages(oldCarImages);
+      }
+
       const userId = req.user.userId;
-      const imagePaths = req.files.map(
-        (file) => Date.now() + file.originalname
-      );
       const featuresArray = JSON.parse(Features);
 
-      const newCar = new Cars({
+      const updatedCar = await Cars.findByIdAndUpdate(carId, {
         make,
         model,
         manufacturing_year,
         is_available,
         price,
         User: userId,
-        images:imagePaths,
+        images: imagePaths,
         description,
         location,
         Features: featuresArray,
       });
 
-      await newCar.save();
+      if (!updatedCar) {
+        return res.status(404).json({ error: "Car not found" });
+      }
 
-      res.json({ message: "Successfully added New Car." });
+      return res.json({ message: "Car updated successfully", updatedCar });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Internal Server Error" });
     }
+  });
+};
+
+export const GetCarDetails = async (req, res) => {
+  const carId = req.params.id;
+
+  try {
+    const car = await Cars.findById(carId).populate("Features");
+
+    if (!car) {
+      return res.status(404).json({ error: "Car not found" });
+    }
+
+    return res.json({ car });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-});
 };
-
-export const GetAllCars = async (req, res) => {
-    try {
-        const page = parseInt(req.query.page) || 1;
-        const pageSize = parseInt(req.query.pageSize) || 10; 
-    
-        const totalCars = await Cars.countDocuments();
-        const totalPages = Math.ceil(totalCars / pageSize);
-    
-        if (page > totalPages) {
-          return res.status(404).json({ error: 'Page not found' });
-        }
-    
-        const cars = await Cars.find().populate('Features')
-          .skip((page - 1) * pageSize)
-          .limit(pageSize);
-    
-        res.status(200).json({
-          page,
-          totalPages,
-          pageSize,
-          totalCars,
-          cars,
-        });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      }  
-};
-
-export const UpdateCar = async (req, res) => {};
 
 export const DeleteCar = async (req, res) => {
-    try {
-        const carId = req.params.id;
-    
-        const existingCar = await Cars.findById(carId);
-        if (!existingCar) {
-          return res.status(404).json({ error: 'Car not found' });
-        }
-    
-        await existingCar.remove();
-    
-        res.json({ message: 'Car deleted successfully' });
-      } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const carId = req.params.id;
+
+    const existingCar = await Cars.findById(carId);
+    if (!existingCar) {
+      return res.status(404).json({ error: "Car not found" });
+    }
+
+    await existingCar.remove();
+
+    res.json({ message: "Car deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const DeleteImages = async (imagePaths) => {
+  try {
+    for (const imagePath of imagePaths) {
+      const fullPath = path.join("./public/uploads/carimages", imagePath);
+
+      console.log(fullPath);
+
+      if (fs.existsSync(fullPath)) {
+        fs.unlinkSync(fullPath);
+      } else {
+        console.log(`File not found: ${fullPath}`);
       }
+    }
+  } catch (error) {
+    console.error("Error deleting images:", error);
+    throw error;
+  }
 };
 
 export const AddFeature = async (req, res) => {
@@ -171,15 +333,15 @@ export const AddFeature = async (req, res) => {
     await newFeature.save();
     res.status(201).json({ message: "Successfully added new feature." });
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-export const listAllFeatures = async (req, res) => {
+export const ListAllFeatures = async (req, res) => {
   try {
     const features = await Features.find();
     res.status(200).json({ features: features });
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- src/routes/car.js
+++ src/routes/car.js
@@ -1,13 +1,16 @@
 // src/routes/auth.js
 import { Router } from 'express';
-import { AddCar, AddFeature, GetAllCars, listAllFeatures } from '../controllers/carController.js';
+import { AddCar, AddFeature, GetAllCars, ListAllFeatures, GetAllCarsImages, GetCarDetails, UpdateCar } from '../controllers/carController.js';
 import { authenticate } from '../middlewares/authMiddleware.js';
 
 const car = Router();
 
 car.post('/add-car', authenticate, AddCar);
+car.post('/update-car/:id', authenticate, UpdateCar);
 car.get('/get-cars', authenticate, GetAllCars);
+car.get('/get-car/:id', authenticate, GetCarDetails);
+car.get('/get-all-car-images/:imageName', authenticate, GetAllCarsImages);
 car.post('/add-feature', authenticate, AddFeature);
-car.get('/get-all-features', authenticate, listAllFeatures);
+car.get('/get-all-features', authenticate, ListAllFeatures);
 
 export default car;
