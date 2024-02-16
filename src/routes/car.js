3323ebbd40132e63d05a9fe2537b9a6257236e38
Alim.dev <alimjivani.dev@gmail.com>
2024-02-16 13:46:00 -0500

    Added names of function in car route

--- src/routes/car.js
+++ src/routes/car.js
@@ -8,6 +8,10 @@ import {
   GetCarDetails,
   UpdateCar,
   DeleteCar,
+  AddAvailability,
+  ListAvailability,
+  DeleteAvailability,
+  ModifyAvailability,
 } from "../controllers/carController.js";
 import { authenticate } from "../middlewares/authMiddleware.js";
 import multer from "multer";
@@ -64,10 +68,10 @@ car.post("/add-feature", authenticate, AddFeature);
 car.delete("/delete-car/:id", authenticate, DeleteCar);
 car.get("/get-all-features", authenticate, ListAllFeatures);
 
-// Car Availability 
-car.post('/add-availability', AddAvailability);
-car.get('/list-availability', ListAvailability);
-car.delete('/delete-availability', DeleteAvailability);
-car.put('/modify-availability', ModifyAvailability);
+// Car Availability
+car.post("/add-availability", AddAvailability);
+car.get("/list-availability", ListAvailability);
+car.delete("/delete-availability", DeleteAvailability);
+car.put("/modify-availability", ModifyAvailability);
 
 export default car;
