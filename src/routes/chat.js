97a6859ad6ae63309f464c926c664ef835fc905c
Rohan Bhalara <bhalararohan88@gmail.com>
2024-02-23 15:55:53 -0500

    feat: Chat implemented

---
+++ src/routes/chat.js
@@ -0,0 +1,16 @@
+// src/routes/auth.js
+import { Router } from "express";
+import { CreateChatList, DeleteChatLists, GetChatLists, ListMessages, SendMessage } from "../controllers/chatController.js";
+import { authenticate } from "../middlewares/authMiddleware.js";
+
+const chat = Router();
+
+chat.post("/send-message", SendMessage);
+chat.get("/get-messages", ListMessages);
+
+// Chat List API
+chat.post("/create-chat-list", CreateChatList);
+chat.get("/get-chat-list", GetChatLists);
+chat.delete("/delete-chat-list/:id", DeleteChatLists);
+
+export default chat;
