d3a3eecbc5a2df61df7c9865f1780bd48c615898
Alim.dev <alimjivani.dev@gmail.com>
2024-03-08 18:35:54 -0500

    change date format and also changed delete availability function

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -326,7 +326,7 @@ export const AddAvailability = async (req, res) => {
       return res.status(404).json({ error: "Car not found" });
     }
 
-    const invalidDates = dates.filter(date => !/^\d{2}-\d{2}-\d{4}$/.test(date));
+    const invalidDates = dates.filter(date => !/^\d{2}\/\d{2}\/\d{4}$/.test(date));
 
     if (invalidDates.length > 0) {
       return res.status(400).json({ error: "Invalid date format. Date should be in mm-dd-yyyy format." });
@@ -372,7 +372,7 @@ export const ListAvailability = async (req, res) => {
 
 export const DeleteAvailability = async (req, res) => {
   try {
-    const { car_id, dates } = req.body;
+    const { car_id } = req.query;
     // Find the car by ID
     const car = await Cars.findById(car_id);
 
